911d7523fc920a2728a1bb6e9276574a1cda9ab9
wq <wei.qiang@successinfo.com.cn>
2018-08-06 07:29:11 +0800

    品优购

---
+++ pyg-parent/pyg-eureka/src/main/java/com/pyg/eureka/PygEurekaApp.java
@@ -0,0 +1,13 @@
+package com.pyg.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class PygEurekaApp {
+	public static void main(String[] args) {
+		SpringApplication.run(PygEurekaApp.class, args);
+	}
+}
