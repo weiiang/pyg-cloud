7dd7a928ced6698dbc3e11842691b397319058ad
weiqiang <1432114216@qq.com>
2018-08-20 09:55:25 +0800

    引入swagger文档

--- pyg-parent/pyg-seller-goods-service/src/main/java/com/pyg/manage/controller/BrandController.java
+++ pyg-parent/pyg-seller-goods-service/src/main/java/com/pyg/manage/controller/BrandController.java
@@ -1,5 +1,6 @@
 package com.pyg.manage.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -17,6 +18,10 @@ import com.pyg.entity.TbBrand;
 import com.pyg.entity.TbBrandExample;
 import com.pyg.service.BrandService;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/brand")
 public class BrandController extends BaseController{
@@ -25,19 +30,22 @@ public class BrandController extends BaseController{
 
 	@Autowired
 	private BrandService brandService;
-	
+	@ApiOperation(value = "查询全部品牌信息列表", notes = "返回品牌信息list,可以根据属性查询")
 	@GetMapping("/list")
 	public Page<TbBrand> list(TbBrandExample brandExample){
 		return brandService.pageList(brandExample);
 	}
-	
+	@ApiOperation(value = "分页查询,默认每页十条数据", notes = "可根据Map封装参数进行条件查询")
 	@GetMapping("/page")
 	public Page<Map<String, Object>> page(){
 		logger.info("provider--->queryMap[ "+this.getParams().toString()+" ]");
 		return brandService.getPage(this.getParams());
 	}
 	
-	
+	@ApiOperation(value = "根据ID查询品牌信息", notes = "type=1查询详情,type=2,回显编辑的信息")
+	@ApiImplicitParams({//传递的参数说明,该类型的参数是拼接在url后面的
+		@ApiImplicitParam(name = "id", value = "id", required = true, dataType = "long", paramType = "query"),
+		@ApiImplicitParam(name = "type", value = "type", required = true, dataType = "int", paramType = "query") })
 	@GetMapping("/info")
 	public Result getInfo(int type, long id){
 		Result result = null;
@@ -51,6 +59,7 @@ public class BrandController extends BaseController{
 		}
 	}
 	
+	@ApiOperation(value = "编辑新增通用API", notes = "当ID为空的时候新增,ID不为空的时候修改")
 	@PostMapping("/edit")
 	public Result edit(TbBrand tbBrand) {
 		try {
@@ -59,6 +68,20 @@ public class BrandController extends BaseController{
 		} catch (Exception e) {
 			Result result = Result.failure(500, e.getMessage(), null);
 			logger.error("修改品牌信息失败<--->result:"+result.toString());
+			return result;
+		}
+		
+	}
+	
+	@ApiOperation(value = "单个删除/批量删除通用API", notes = "当List只有一条数据删除单个,当多条数据逐个删除")
+	@PostMapping("/del")
+	public Result del(List<Long>[] ids) {
+		try {
+			Integer delCount = brandService.delBeatch(ids);
+			return Result.success(delCount);
+		} catch (Exception e) {
+			Result result = Result.failure(500, e.getMessage(), null);
+			logger.error("删除品牌信息失败<--->result:"+result.toString());
 			return result;
 		}
 		
