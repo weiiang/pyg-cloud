2feee4c70c608ce5d5f8265264e728595cfcd8ef
weiqiang <1432114216@qq.com>
2018-09-13 18:25:38 +0800

    guolvqi

---
+++ pyg-parent/pyg-zuul/src/main/java/com/pyg/zuul/controller/ErrorControllerHandler.java
@@ -0,0 +1,13 @@
+package com.pyg.zuul.controller;
+/**
+ *author: 		wq
+ *datetime: 	2018年9月13日	下午6:11:47
+ *E-mail:		1432114216@qq.com
+ *blog:			https://blog.csdn.net/weiqiang2
+ *Description:	
+ *
+ */
+public class ErrorControllerHandler {
+
+}
+
